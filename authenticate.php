bbdd7bf0509ec56cdf86a8d1d58924faa58c5a8f
Itscominghome12 <Cp92@mahurangi.school.nz>
2022-08-23 10:55:36 +1200

    version 11
    Login as well as registartion now functional

--- authenticate.php
+++ authenticate.php
@@ -2,17 +2,9 @@
 				<?php
 session_start();
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'Canopydb';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 
+
+include("setup.php");
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['username'], $_POST['password']) ) {
@@ -21,14 +13,14 @@ if ( !isset($_POST['username'], $_POST['password']) ) {
 }
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+if ($stmt = $conn->prepare('SELECT id, password,admin FROM accounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
 	if ($stmt->num_rows > 0) {
-		$stmt->bind_result($id, $password);
+		$stmt->bind_result($id, $password, $admin);
 		$stmt->fetch();
 		// Account exists, now we verify the password.
 		// Note: remember to use password_hash in your registration file to store the hashed passwords.
@@ -39,8 +31,13 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 			$_SESSION['loggedin'] = TRUE;
 			$_SESSION['name'] = $_POST['username'];
 			$_SESSION['id'] = $id;
-			header('Location: index.php');
-            $_SESSION['message'] = "test";
+            $_SESSION['admin'] = $admin;
+             $_SESSION['message'] = "test";
+            if($admin==1){
+			header('Location: admin.php');}
+            else 
+            {header('Location: index.php');}}
+           
 		} else {
 			// Incorrect password
 			echo 'Incorrect username and/or password!';
