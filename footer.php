f3e56720fb4f48fe753c6ac7f2211be4eb096b37
Itscominghome12 <Cp92@mahurangi.school.nz>
2022-11-01 10:24:13 +1300

    version 14 1/11/22

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer>
+        <div class="footer-content">
+            <h3>Kia ora Staff</h3>
+            <p>Feel free to download the app version of this website. There are links to the website from selected social media pages. For any help or contact information please email Canopy@gmail.com <a href="https://mail.google.com/mail/u/0/#inbox"></a>Canopy@gmail.com</p>
+            <ul class="socials">
+                <li><a href="https://www.facebook.com/CanopyCancerCare/"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+            
+            
+      
+            
+        </div>
+        <div class="footer-bottom">
+            <p>copyright &copy;2022 <a href="#">Canopy Cancer Care</a>  </p>
+                    <div class="footer-menu">
+                      <ul class="f-menu">
+                        <li><a href="index.php">Home</a></li>
+     <li><a href="epsom.php">Epsom</a></li>
+                        <li><a href="tauranga.php">Tauranga</a></li>
+                        <li><a href="">North Shore</a></li>
+                          <li><a href="">Whangarei</a></li>
+                          <li><a href="">Hawkes Bay</a></li>
+                      </ul>
+                    </div>
+        </div>
+
+    </footer>
+
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html>
 
-<?php include("headnav.html")?>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+    
+<body>
+
+<?php include("headnav.php")?>
 
 <?php include("setup.php")?>
 
 <?php include("carousel.html")?>
+    
 
-<?php include("footer.html")?>
+<?php include("footer.php")?>
+    </body>
     
 </html>
